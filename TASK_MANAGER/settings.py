69d8e25dd6e93e984e3fb209b637c15456da5683
J-Krisz <jkristi4n@gmail.com>
2021-09-01 15:06:13 +0100

    remove secret key

--- TASK_MANAGER/settings.py
+++ TASK_MANAGER/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import psycopg2
 import os
+import secret
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6a6m(#5h6b6tv++)n!r(tfg*=hu64@pxx)=gl1w$m&zt6^at(@'
-
+SECRET_KEY = secret.KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
